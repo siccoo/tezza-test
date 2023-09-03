ae15513c4be7b53e085afbbda3114957917d4bff
Michael Chilaka <obichilaka@gmail.com>
2023-09-03 19:05:28 +0100

    finishing touches

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 import Logo from "../../assets/images/logo.png";
 
-import {GrAppsRounded, GrTask, GrScheduleNew} from "react-icons/gr";
-import {FaUsers} from "react-icons/fa";
-import {AiOutlineMessage} from "react-icons/ai";
-import {TbReportAnalytics, TbReportMoney} from "react-icons/tb";
+import { GrAppsRounded, GrTask, GrScheduleNew } from "react-icons/gr";
+import { FaUsers } from "react-icons/fa";
+import { AiOutlineMessage } from "react-icons/ai";
+import { TbReportAnalytics, TbReportMoney } from "react-icons/tb";
 
-import { Layout, Menu, } from "antd";
+import { Layout, Menu } from "antd";
 import Topbar from "../Topbar";
 
 const { Content, Sider } = Layout;
@@ -22,14 +22,16 @@ function getItem(label, key, icon, children) {
   };
 }
 
-
-
 const items = [
   getItem("Workspace", "1", <GrAppsRounded className="side-icons" />),
   getItem("Clients", "2", <FaUsers className="side-icons" />),
-  getItem("Schedule Appointment", "3", <GrScheduleNew className="side-icons" />),
+  getItem(
+    "Schedule Appointment",
+    "3",
+    <GrScheduleNew className="side-icons" />
+  ),
   getItem("Payment", "4", <TbReportMoney className="side-icons" />),
-  getItem("Task", "5", <GrTask className="side-icons" /> ),
+  getItem("Task", "5", <GrTask className="side-icons" />),
   getItem("Message", "6", <AiOutlineMessage className="side-icons" />),
   getItem("Reports", "sub2", <TbReportAnalytics className="side-icons" />, [
     getItem("Team 1", "7"),
@@ -59,10 +61,26 @@ const SidebarLayout = ({ children }) => {
             if (item.label === "Workspace" || item.label === "Clients") {
               return (
                 <Menu.Item key={item.key} icon={item.icon}>
-                  <Link to={`/${item.label.toLowerCase()}`}>
-                    {item.label}
-                  </Link>
+                  <Link to={`/${item.label.toLowerCase()}`}>{item.label}</Link>
                 </Menu.Item>
+              );
+            } else if (item.label === "Reports") {
+              return (
+                <Menu.SubMenu
+                  key={item.key}
+                  icon={item.icon}
+                  title={item.label}
+                >
+                  {item.children.map((subItem) => (
+                    <Menu.Item key={subItem.key}>
+                      <Link
+                        to={`/${subItem.label.toLowerCase().replace(" ", "-")}`}
+                      >
+                        {subItem.label}
+                      </Link>
+                    </Menu.Item>
+                  ))}
+                </Menu.SubMenu>
               );
             }
             return (
